038fd1972d82a344fc95f9fd2a440b8529ea807e
TheNyanChicken <chickennyan2@gmail.com>
2021-11-01 13:09:03 +0800

    Added listing all events and ability to delete events

--- pyreah.py
+++ pyreah.py
@@ -5,21 +5,18 @@ import datetime
 
 #variables
 time = datetime.datetime.now().time()
-UserData = {
-    "name":""
-    }
-
+UserData = {}
 UserEvents = []
 
 #variables for the main window
 root = tk.Tk()
 root.title("Pyreah")
+root.resizable(False, False)
 root.geometry("300x300")
 wrap_length = 300
 options = [
     "Calender"
     ]
-
 
 #this creates a new/updates save files
 def save_data():
@@ -107,9 +104,9 @@ def calender_add_new_event_window():
     event_title_input = tk.Entry(calender_add_new_event_frame)
     event_title_input.pack()
     tk.Label(calender_add_new_event_frame, text="Date of Event (Day, Month)").pack()
-    event_day_input = tk.Entry(calender_add_new_event_frame, textvariable=tk.IntVar(), text="DD", width="2")
+    event_day_input = tk.Entry(calender_add_new_event_frame, textvariable=tk.IntVar(), width="2")
     event_day_input.pack(side=tk.LEFT, padx="10")
-    event_month_input = tk.Entry(calender_add_new_event_frame, textvariable=tk.IntVar(),  text="MM", width="2")
+    event_month_input = tk.Entry(calender_add_new_event_frame, textvariable=tk.IntVar(), width="2")
     event_month_input.pack(side=tk.LEFT, padx="10")
     event_confirm = tk.Button(calender_add_new_event_frame, text="Confirm")
     event_confirm.pack()
@@ -117,10 +114,10 @@ def calender_add_new_event_window():
         event_day = int(event_day_input.get())
         event_month = int(event_month_input.get())
         if event_day >= 1 and event_day <= 31 and event_month >= 1 and event_month <= 12:
-            event_info = {'Event Title':event_title_input.get(), 'Event Day':event_day, 'Event Month':event_month}
+            event_info = {'event_title':event_title_input.get(), 'event_day':event_day, 'event_month':event_month}
             UserEvents.append(event_info)
-            for event in UserEvents:
-                print(event)
+            calender_add_new_event_frame.destroy()
+            calender_window()
     event_confirm.bind("<Button-1>", onclick_confirm)
     back = tk.Button(calender_add_new_event_frame, text="Back")
     back.pack()
@@ -133,9 +130,27 @@ def calender_add_new_event_window():
 #window for seeing all current events
 def calender_view_events_window():
     calender_view_events_frame = tk.Frame(root)
-    tk.Label(calender_view_events_frame, text="Viewing all events").pack()
+    event_number = 1
+    tk.Label(calender_view_events_frame, text="These are all the events you currently have planned:", wraplength=wrap_length).pack()
+    scrollbar = tk.Scrollbar(calender_view_events_frame, orient='vertical')
+    scrollbar.pack(side="right", fill="y")
+    view_events_list = tk.Listbox(calender_view_events_frame, yscrollcommand = scrollbar.set)
+    for events in UserEvents:
+        event_text = str(event_number) + ". " + events['event_title'] + " at " + str(events['event_day']) + "/" + str(events['event_month'])
+        view_events_list.insert('end', event_text)
+        event_number = event_number + 1
+    scrollbar.config(command=view_events_list.yview,)
+    view_events_list.pack(side='left', fill='both')
+    delete = tk.Button(calender_view_events_frame, text="Delete Event")
+    delete.pack(side="top")
+    def onclick_delete(e):
+        selection = view_events_list.curselection()
+        del UserEvents[selection[0]]
+        view_events_list.delete(selection[0])
+        save_data()
+    delete.bind("<Button-1>", onclick_delete)
     back = tk.Button(calender_view_events_frame, text="Back")
-    back.pack(side=tk.BOTTOM)
+    back.pack(side="bottom")
     def onclick_back(e):
         calender_view_events_frame.destroy()
         calender_window()
@@ -147,7 +162,7 @@ def about_me_window():
     about_frame = tk.Frame(root)
     tk.Label(about_frame, text="Hi, I'm Pyreah, a simple digital assistant made by TheNyanChicken. I can handle simple tasks such as reminding you of important events and scheduling. With the addition of any other optional modules that can be gotten at <insert hyperlink here>, I can do other things as well. I hope to be of good service.", wraplength=wrap_length).pack()
     back = tk.Button(about_frame, text="Back")
-    back.pack(side=tk.BOTTOM)
+    back.pack()
     def onclick_back(e):
         about_frame.destroy()
         main_window()
